4d5a61614bf4b40655d04fe74734f509cefc8e27
safdevke <safdevke@gmail.com>
2021-08-31 07:57:39 +0300

    Initial Commit

---
+++ src/main/java/systech/cohort/nine/App.java
@@ -0,0 +1,19 @@
+package systech.cohort.nine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println();
+        System.out.println();
+        System.out.println("==================================");
+        System.out.println( "This application is for review or recap of java basics for systech cohort 9" );
+        System.out.println("=================================");
+        System.out.println();
+        System.out.println();
+    }
+}
